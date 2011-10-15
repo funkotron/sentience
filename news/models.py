0e2d7214462292fb36d7e6dd9fca44ffe6f3eb0d
Adam Awan <adamja1@gmail.com>
2011-10-15 13:28:22 +0100

    Article spider work

--- news/models.py
+++ news/models.py
@@ -3,11 +3,13 @@ from django.db import models
 class Entity(models.Model):
     name = models.CharField(max_length=255)
     ticker = models.CharField(max_length=20)
+    exchange = models.CharField(max_length=20)
 
 class Article(models.Model):
     name = models.CharField(max_length=255)
     entity = models.ForeignKey(Entity)
-    datetime = models.DateField()
+    src = models.CharField(max_length=255)
+    date = models.DateField()
     body = models.TextField()
 
 class StockPrice(models.Model):
--- news/spider.py
+++ news/spider.py
@@ -1,3 +1,4 @@
+from sentience.news.models import Entity,Article
 import re
 import urllib
 import BeautifulSoup
@@ -9,12 +10,16 @@ class Spider():
     def __init__(self, 
                  entity=None, 
                  start=datetime.now()-timedelta(days=60), 
-                 end=datetime.now()):
-        self.entity = entity
-        self.start = start
+                 end=datetime.now(),
+                 chunk_size = 50):
+        self.date_range = []
+        days = (end - start).days()
+        for i in range(days):
+            self.date_range.append(
         self.end = end
+        self.chunk_size = chunk_size #How many results to return in one request
 
-    def get_visible(url):
+    def get_visible(self,url):
         #Return the visible text on a page
         #Adapted from http://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
         html = urllib.urlopen(url).read()
@@ -31,8 +36,32 @@ class Spider():
         visible_texts = filter(visible, texts)
         return visible_texts
 
-    def find_articles(entity,start,end):
+    def find_articles(self)
         #Return any article urls related to an entity between two given dates
-        return None
+        url = "http://www.google.co.uk/finance/company_news?q=%s:%s&start=%d&num=%d"%(self.entity.exchange,
+                                                                                      self.entity.ticker,
+                                                                                      start_count,
+                                                                                      self.chunk_size)
+        html = urllib.urlopen(url).read()
+        soup = BeautifulSoup.BeautifulSoup(html)
+        #TODO iterate over this
+        article_list = soup.findAll("div", {"class":"g-section news sfe-break-bottom-16"})
+        for i in article_list:
+            link = i.find("span",{"class":"name"}).a
+            name = link.text
+            url = link.attrs[0]
+            date_text = i.find("span",{"class":"date"}).text
+            date = datetime.strptime(date_text,"%b %d, %Y")
+            src = i.find("span",{"class":"src"}).text
+            body = self.get_visible(url)
+            article = Article(name=name,
+                              entity=self.entity,
+                              src=src,
+                              date = date,
+                              body = body)
+            article.save()
+
+
+        return True
 
 
